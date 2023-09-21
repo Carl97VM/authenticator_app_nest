b483df5dc7fca9c547b351703efa1cf1851cdf46
Carl97VM <barcacarlosv@gmail.com>
2023-09-21 10:52:49 -0400

    insercion y validacion en el modelo usuarios

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './entities/user.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+  constructor(
+    @InjectModel( User.name ) 
+    private userModel: Model<User>
+  ){}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    // console.log(createUserDto);
+    // Se instancia el modelo y se manda los datos para insertar en la base de datos
+    // Se tienen que hacer validaciones para poder encriptar, poner tokens y demas
+    // Los errores de backend ya esta identificados mediante el catch utilizar error.code y identificar que paso
+    try {
+      const newUser = new this.userModel( createUserDto )
+      return await newUser.save()
+    } catch (error) {
+      if( error.code === 11000 ){
+        throw new BadRequestException(`${ createUserDto.email } already exists!`)
+      }
+      throw new InternalServerErrorException('Something terrible happend!!!')
+    }
   }
 
   findAll() {
@@ -16,7 +37,7 @@ export class AuthService {
     return `This action returns a #${id} auth`;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
+  update(id: number, updateAuthDto: UpdateUserDto) {
     return `This action updates a #${id} auth`;
   }
 
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+
+@Schema()
+export class User {
+    // _id: string;
+    @Prop( {unique:true, required: true} )
+    email: string;
+    @Prop( {default: true} )
+    isActive: boolean;
+    @Prop( {minlength: 6, required: true} )
+    name: string;
+    @Prop( {default: true} )
+    password: string;
+    @Prop( {type: [String], default: ['user']} )
+    roles: string[];
+}
+
+// para mandar a mongos
+export const UserSchema = SchemaFactory.createForClass( User );
+
